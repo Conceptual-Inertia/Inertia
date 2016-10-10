bc6a6cc858a55622a08ab4a0f8c813655645e430
tylersiqi <tylersiqi@163.com>
2016-10-10 16:51:46 -0400

    debug 1

--- src/main.c
+++ src/main.c
@@ -33,16 +33,18 @@
 
 unsigned regs[ NUM_REGS ];
 unsigned memory [ NUM_MEMS ];
-unsigned long program[] = { 0x06800000, 0x00000007, 0x00000008, 0xA4000000, 0xE0000000 };
+unsigned program[] = { 0x66800000, 0x00000000, 0x00000007, 0x00000008, 0xA4000000, 0x00000000, 0xE0000000, 0x00000000 };
 unsigned cons[3];
 
 /* fetch the next word from the program */
-long fetch(int *pc) {
-    return program[*pc++];
+unsigned fetch(int *pc) {
+    (*pc) ++;
+    printf("fetch %d\n", *pc - 1);
+    return program[*pc - 1];
 }
 
 /* instruction fields */
-int instrNum = 0;
+unsigned instrNum = 0;
 int reg1     = 0;//-1 as const, 0-3 as register, >=4 as memory
 int reg2     = 0;
 int reg3     = 0;
@@ -50,11 +52,14 @@ int imm      = 0;
 
 /* fetch and decode a word */
 void decode(int *pc)  {
-    long instr = fetch(*pc);
-    instrNum = instr >> 60;
-    reg1 = (instr >> 58) & 3;//0 as memory, 1 as register, 2 as const
-    reg2 = (instr >> 56) & 3;
-    reg3 = (instr >> 54) & 3;
+    
+    unsigned instr = fetch(pc);
+    instrNum = instr >> 28;
+    //printf("%u", instr >> 28);
+
+    reg1 = (instr >> 26) & 3;//0 as memory, 1 as register, 2 as const
+    reg2 = (instr >> 24) & 3;
+    reg3 = (instr >> 22) & 3;
     if (reg1 == 0) reg1 = 4;
     if (reg2 == 0) reg2 = 4;
     if (reg3 == 0) reg3 = 4;
@@ -62,40 +67,42 @@ void decode(int *pc)  {
     if (reg2 == 2) reg2 = -1;
     if (reg3 == 2) reg3 = -1;
     
+    
     if (reg1 == 1) {
-        reg1 = (instr >> 52) & 3;
+        reg1 = (instr >> 20) & 3;
     }
     
     if (reg2 == 1) {
-        reg2 = (instr >> 50) & 3;
+        reg2 = (instr >> 18) & 3;
     }
     
     if (reg3 == 1) {
-        reg3 = (instr >> 48) & 3;
+        reg3 = (instr >> 16) & 3;
     }
     
     if (reg1 == 4){
-        reg1 = ((instr >> 32) & 65535) + 4;
+        reg1 = (instr & 65535) + 4;
     }
     
+    instr = fetch(pc);
     if (reg2 == 4){
         reg2 = ((instr >> 16) & 65535) + 4;
     }
     
     if (reg3 == 4){
-        reg3 = ((instr >> 0) & 65535) + 4;
+        reg3 = (instr & 65535) + 4;
     }
     
     if (reg1 == -1){
-        cons[0] = (int) fetch(*pc);
+        cons[0] = fetch(pc);
     }
     
     if (reg2 == -1){
-        cons[1] = (int) fetch(*pc);
+        cons[1] = fetch(pc);
     }
     
     if (reg3 == -1){
-        cons[2] = (int) fetch(*pc);
+        cons[2] = fetch(pc);
     }
 }
 
@@ -129,56 +136,57 @@ void eval(int *running, int *pc)
         
         case INERTIA_ADD:
             /* add */
-            *get_add(reg1) = *get_add(reg2) + *get_add(reg3);
+            ( *get_add(1) ) = ( *get_add(2) ) + ( *get_add(3) );
             break;
         case INERTIA_DIV:
             /* divide */
-            *get_add(reg1) = *get_add(reg2) / *get_add(reg3);
+            ( *get_add(1) ) = ( *get_add(2) ) / ( *get_add(3) );
             break;
         case INERTIA_MUL:
             /* multiply */
-            *get_add(reg1) = *get_add(reg2) * *get_add(reg3);
+            ( *get_add(1) ) = ( *get_add(2) ) * ( *get_add(3) );
             break;
             
         case INERTIA_LTN :
             //Less Than
-            if (*get_add(reg2) < *get_add(reg3)) *get_add(reg1) = ~0;
-            else *get_add(reg1) = 0;
+            if (( *get_add(2) ) < ( *get_add(3) )) ( *get_add(1) ) = ~0;
+            else ( *get_add(1) ) = 0;
             break;
         case INERTIA_EQL :
             //Less Than
-            if (*get_add(reg2) == *get_add(reg3)) *get_add(reg1) = ~0;
-            else *get_add(reg1) = 0;
+            if (( *get_add(2) ) == ( *get_add(3) )) ( *get_add(1) ) = ~0;
+            else ( *get_add(1) ) = 0;
             break;
         case INERTIA_AND:
-            *get_add(reg1) = *get_add(reg2) & *get_add(reg3);
+            ( *get_add(1) ) = ( *get_add(2) ) & ( *get_add(3) );
             break;
         case INERTIA_OR:
-            *get_add(reg1) = *get_add(reg2) | *get_add(reg3);
+            ( *get_add(1) ) = ( *get_add(2) ) | ( *get_add(3) );
             break;
         case INERTIA_NOT:
-            *get_add(reg1) = ~*get_add(reg2);
+            ( *get_add(1) ) = ~( *get_add(2) );
             break;
         case INERTIA_SHIFTL:
-            *get_add(reg1) = *get_add(reg2) << *get_add(reg3);
+            ( *get_add(1) ) = ( *get_add(2) ) << ( *get_add(3) );
             break;
         case INERTIA_SHIFTR:
-            *get_add(reg1) = *get_add(reg2) >> *get_add(reg3);
+            ( *get_add(1) ) = ( *get_add(2) ) >> ( *get_add(3) );
             break;
             
         case INERTIA_LOAD:
-            *get_add(reg1) = *get_add(reg2);
+            ( *get_add(1) ) = ( *get_add(2) );
             break;
         case INERTIA_PRINT:
-            printf("%d\n", *get_add(reg1));
+            printf("%d\n", ( *get_add(1) ));
             break;
 	case INERTIA_GOTO:
-            *pc = *get_add(reg1);
+            *pc = ( *get_add(1) );
             break;
         case INERTIA_IF:
-            if (*get_add(reg1) != 0) *pc = *get_add(reg2);
+            if (( *get_add(1) ) != 0) *pc = ( *get_add(2) );
             break;
         case INERTIA_RETURN:
+            //printf("returning");
             *running = 0;
             break;
     }
@@ -200,7 +208,7 @@ void run(int pc)
     while( running )
     {
         decode( &pc );
-        eval( &running );
+        eval( &running , &pc );
     }
 }
 
