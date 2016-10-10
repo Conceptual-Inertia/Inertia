d983b71876aa3e56bb5efc98a1acfab2dc2c12ab
tylerliu <tylersiqi@163.com>
2016-10-09 21:05:56 -0400

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -1,12 +1,16 @@
+//
+//  main.c
+//  Inertia
+//
+//  Created by Tyler Liu on 2016/10/09.
+//  Copyright © 2016 Tyler Liu. All rights reserved.
+//
+
 #include <stdio.h>
 
-#ifndef NUM_REGS
-  #define NUM_REGS 4
-#endif
+#define NUM_REGS 4
 
-#ifnedf NUM_MEMS
-  #define NUM_MEMS 100
-#endif
+#define NUM_MEMS 256
 
 #define INERTIA_ADD 0x0 // Integer Addition
 #define INERTIA_DIV 0x1 // Integer Division
@@ -28,9 +32,10 @@
 #define INERTIA_RETURN 0xF //return
 
 
-unsigned register regs[ NUM_REGS ];
-
+unsigned regs[ NUM_REGS ];
+unsigned memory [ NUM_MEMS ];
 unsigned program[] = { 0x1064, 0x11C8, 0x2201, 0x0000 };
+int cons[3];
 
 /* program counter */
 int pc = 0;
@@ -42,18 +47,116 @@ int fetch() {
 
 /* instruction fields */
 int instrNum = 0;
-int reg1     = 0;
+int reg1     = 0;//-1 as const, 0-3 as register, >=4 as memory
 int reg2     = 0;
 int reg3     = 0;
 int imm      = 0;
 
 /* decode a word */
 void decode( int instr )  {
-    instrNum = (instr & 0xF000) >> 12;
-    reg1     = (instr & 0xF00 ) >>  8;
-    reg2     = (instr & 0xF0  ) >>  4;
-    reg3     = (instr & 0xF   );
-    imm      = (instr & 0xFF  );
+    int dcut = 0;
+    
+    instrNum = instr >> 28;
+    instr = instr << 4;
+    if (((instr >>  31) & 1 )== 0) {
+        reg1 = 4;
+        instr = instr << 1;
+        dcut ++;
+    }//memory
+    else if (((instr >> 30) & 3) == 2) {
+        reg1 = 1;//register
+        instr = instr << 2;
+        dcut += 2;
+    }
+    else {
+        reg1 = -1;
+        instr = instr << 2;
+        dcut += 2;
+    }
+    
+    if (((instr >>  31) & 1 )== 0) {
+        reg2 = 4;
+        instr = instr << 1;
+        dcut ++;
+    }//memory
+    else if (((instr >> 30) & 3) == 2) {
+        reg2 = 1;//register
+        instr = instr << 2;
+        dcut += 2;
+    }
+    else {
+        reg2 = -1;
+        instr = instr << 2;
+        dcut += 2;
+    }
+    
+    if (((instr >>  31) & 1 )== 0) {
+        reg3 = 4;
+        instr = instr << 1;
+        dcut ++;
+    }//memory
+    else if (((instr >> 30) & 3) == 2) {
+        reg3 = 1;//register
+        instr = instr << 2;
+        dcut += 2;
+    }
+    else {
+        reg3 = -1;
+        instr = instr << 2;
+        dcut += 2;
+    }
+    
+    if (reg1 == 1) {
+        reg1 = (instr >> 30);
+        instr = instr << 2;
+        dcut += 2;
+    }
+    
+    if (reg2 == 1) {
+        reg2 = (instr >> 30);
+        instr = instr << 2;
+        dcut += 2;
+    }
+    
+    if (reg3 == 1) {
+        reg3 = (instr >> 30);
+        instr = instr << 2;
+        dcut += 2;
+    }
+    
+    if (dcut == 3){
+        instr = instr << 1;
+    }
+    if (dcut > 4 && dcut < 12){
+        instr = instr << (12 - dcut);
+    }
+    
+    if (reg1 == 4){
+        reg1 = (instr >> 24) + 4;
+        instr = instr << 8;
+    }
+    
+    if (reg2 == 4){
+        reg2 = (instr >> 24) + 4;
+        instr = instr << 8;
+    }
+    
+    if (reg3 == 4){
+        reg3 = (instr >> 24) + 4;
+        instr = instr << 8;
+    }
+    
+    if (reg1 == -1){
+        cons[0] = fetch();
+    }
+    
+    if (reg2 == -1){
+        cons[1] = fetch();
+    }
+    
+    if (reg3 == -1){
+        cons[2] = fetch();
+    }
 }
 
 /* the VM runs until this flag becomes 0 */
@@ -82,7 +185,7 @@ void eval()
     }
 }
 
-/* display all registers as 4-digit hexadecimal words */
+/* display all registers as 8-digit hexadecimal words */
 void showRegs()
 {
     int i;
