5fb894498b7e0aaa0f5916336d00b7e69a04aba9
Alex Fang <frjalex@temple.edu>
2016-11-02 13:14:33 -0400

    Change source file version for PLUG talk

---
+++ md/CMakeFiles/Inertia.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Inertia.dir/src/main.c.o"
+  "Inertia.pdb"
+  "Inertia"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Inertia.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
